e2a0e2308bc2b345a84f35b4205dc70da39d300c
Uky <95773741+LordUky@users.noreply.github.com>
2023-04-05 02:26:05 +0800

    Update LoginPage.py

--- LoginPage.py
+++ LoginPage.py
@@ -11,12 +11,12 @@ class LoginPage(object):
         self.page = Frame(self.root)  # 创建Frame
         self.page.pack()
         Label(self.page).grid(row=0, stick=W)
-        Label(self.page, text='账户: ').grid(row=1, stick=W, pady=10)
+        Label(self.page, text='username: ').grid(row=1, stick=W, pady=10)
         Entry(self.page, textvariable=self.username).grid(row=1, column=1, stick=E)
-        Label(self.page, text='密码: ').grid(row=2, stick=W, pady=10)
+        Label(self.page, text='password: ').grid(row=2, stick=W, pady=10)
         Entry(self.page, textvariable=self.password, show='*').grid(row=2, column=1, stick=E)
-        Button(self.page, text='登陆', command=self.loginCheck).grid(row=3, stick=W, pady=10)
-        Button(self.page, text='退出', command=self.page.quit).grid(row=3, column=1, stick=E)
+        Button(self.page, text='Login', command=self.loginCheck).grid(row=3, stick=W, pady=10)
+        Button(self.page, text='Quit', command=self.page.quit).grid(row=3, column=1, stick=E)
 
     def loginCheck(self):
         from FirstPage import FirstPage
