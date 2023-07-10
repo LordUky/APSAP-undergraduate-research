01e7f60f7151c7f86ff84007fe277b1f84c36804
ericericericjin <jyseric@126.com>
2023-07-10 12:40:56 +0400

    Added use arg to set com port

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import gui
 from cam_ctrl import cam_ctrl
 from scale_read import scale_read
+import sys
 
 class assistants():
     def __init__(self) -> None:
         self.pm = gui.models.photo_manager(r"photo_folder/N")
         self.cc = cam_ctrl.cameraControl(tmp_dir="tmp/")
-        self.sr = scale_read.scaleRead('COM9')
+        com_port = sys.argv[1]
+        self.sr = scale_read.scaleRead(com_port)
         self.cp = 0 # current page. 0:login; 1:firstPage; ......
 
 gui.views.GUImain.GUImain(assistants())
